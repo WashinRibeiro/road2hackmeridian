ff4306ebe45cf85adf5efc0c97b6e40ab8124388
Washington Ribeiro <washinuchiha@gmail.com>
2025-08-26 19:48:47 -0300

    fix(desafio-aula1): correcting logarithm and power functions

--- workshop1/aula1/calculator/src/calc3.rs
+++ workshop1/aula1/calculator/src/calc3.rs
@@ -1,7 +1,7 @@
-pub fn potencia(base: f64, exponent: i32) -> f64 {
+pub fn pot(base: f64, exponent: i32) -> f64 {
     base.powi(exponent)
 }
 
-pub fn logaritmo(base: f64, value: f64) -> f64 {
-    value.log(base)
+pub fn log(base: f64, value: f64) -> f64 {
+    value.ln() / base.ln()
 }
