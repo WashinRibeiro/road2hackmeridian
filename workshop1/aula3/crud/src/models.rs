90cfaeb0da72393261297040e74cad96563d56b2
Washington Ribeiro <washinuchiha@gmail.com>
2025-09-04 07:32:43 -0300

    feat(aula3): evolving crud from aula2 to consume the webassembly module on the server

---
+++ workshop1/aula3/crud/src/models.rs
@@ -0,0 +1,9 @@
+// Este struct representa um registro de dados do nosso CRUD.
+// Ele será convertido automaticamente para JSON usando Serde.
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DataEntry {
+    pub func_names: Vec<String>, // Lista de textos
+    pub bytecode: Vec<u8>,     // Lista de números inteiros (bytes)
+}
